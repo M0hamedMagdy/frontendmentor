eee1f562297ef0678241c880aa2babd392582868
unknown <mohamedmagdy12@outlook.com>
2022-07-03 09:23:54 +0200

    refactoring

--- advice-generator-app/app.js
+++ advice-generator-app/app.js
@@ -1,26 +1,25 @@
+
+
 const resDiv = document.querySelector("#advice");
 const resBtn = document.querySelector("#diceBtn");
 const resId = document.querySelector("#adviceNamber");
 
-resBtn.addEventListener("click", () => {
-  getAdvice();
-});
+resBtn.addEventListener("click", getAdvice);
 
-window.onload = () => {
-  getAdvice();
-};
 
-function getAdvice() {
-  fetch(" https://api.adviceslip.com/advice ")
+async function getAdvice() {
+  try {
+    fetch(" https://api.adviceslip.com/advice ")
     .then((response) => {
       return response.json();
     })
     .then((adviceData) => {
+      console.log(adviceData);
       const Adviceobj = adviceData.slip;
       resId.innerHTML = `<h6 id="adviceNamber"> Advice    ${Adviceobj.id} </h6>`;
       resDiv.innerHTML = `<p> ${Adviceobj.advice} </p>`;
     })
-    .catch((error) => {
-      console.log(error);
-    });
+  } catch(error) {
+   console.error(error);
+  }
 }
